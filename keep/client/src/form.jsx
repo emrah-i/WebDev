7c7e0168c39cdc508530ac2675747a1c13d6a8a9
emrah-i <ibraem1026@gmail.com>
2023-08-15 15:52:59 -0400

    Changed the css

--- keep/client/src/form.jsx
+++ keep/client/src/form.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import AddIcon from '@mui/icons-material/Add';
 
 function form() {
 
     return (<form className="add-form" action="/add-note" method="post">
                 <input type='text' name='title' className="form-control" placeholder="Title" maxLength="140" required /><br />
                 <input type='text' name='note' className="form-control" placeholder="Enter a Note..." maxLength="140" required /><br />
-                <button className="btn add-btn">Add</button>
+                <button className="btn add-btn"><AddIcon /></button>
             </form>)
 }
 
